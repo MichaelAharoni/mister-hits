c16db49015d314055c46af3023bd7ddfe33feeae
MichaelAharoni <michaelaharoni001@gmail.com>
2022-06-07 08:53:46 +0300

    sw

--- public/sw.js
+++ public/sw.js
@@ -13,23 +13,24 @@ self.addEventListener('push',(ev)=>{
       })
   })
 
-  self.addEventListener('notificationclick', function(event) {
-    let url = 'https://mister-hits.herokuapp.com/';
-    event.notification.close(); // Android needs explicit close.
-    event.waitUntil(
-        clients.matchAll({type: 'window'}).then( windowClients => {
-            // Check if there is already a window/tab open with the target URL
-            for (var i = 0; i < windowClients.length; i++) {
-                var client = windowClients[i];
-                // If so, just focus it.
-                if (client.url === url && 'focus' in client) {
-                    return client.focus();
-                }
-            }
-            // If not, then open the target URL in a new window/tab.
-            if (clients.openWindow) {
-                return clients.openWindow(url);
-            }
-        })
-    );
+  
+self.addEventListener('notificationclick', function(event) {
+  let url = 'https://mister-hits.herokuapp.com/';
+  event.notification.close(); // Android needs explicit close.
+  event.waitUntil(
+      self.clients.matchAll({type: 'window'}).then( windowClients => {
+          // Check if there is already a window/tab open with the target URL
+          for (var i = 0; i < windowClients.length; i++) {
+              var client = windowClients[i];
+              // If so, just focus it.
+              if (client.url === url && 'focus' in client) {
+                  return client.focus();
+              }
+          }
+          // If not, then open the target URL in a new window/tab.
+          if (self.clients.openWindow) {
+              return self.clients.openWindow(url);
+          }
+      })
+  );
 });
